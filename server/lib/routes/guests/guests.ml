41a1368d777837f04b750159515769b68739f076
Jake Quinter <hello@jakequinter.io>
2024-08-26 15:25:38 -0500

    delete guest

--- server/lib/routes/guests/guests.ml
+++ server/lib/routes/guests/guests.ml
@@ -2,5 +2,6 @@ let routes pool =
   [ Dream.get "/api/guests" (Get_guests.handler pool)
   ; Dream.post "/api/guests" (Add_guest.handler pool)
   ; Dream.get "/api/guests/:id" (Get_guest.handler pool)
+  ; Dream.delete "/api/guests/:id" (Delete_guest.handler pool)
   ]
 ;;
