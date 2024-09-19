2a5994901db9c8870a808faa02f4908efad29162
Jake Quinter <hello@jakequinter.io>
2024-09-19 18:02:52 -0500

    login form

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./_build/default/src/output/src/**/*.{js, jsx, ts, tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: "#f6f6f5",
+          100: "#e7e7e6",
+          200: "#d1d1d0",
+          300: "#b1b0af",
+          400: "#8a8a86",
+          500: "#6f6f6b",
+          600: "#5e5d5c",
+          700: "#504f4e",
+          800: "#464644",
+          900: "#343433",
+          900: "#262626",
+        },
+      },
+      animation: {
+        spin: "spin 1.5s linear infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
