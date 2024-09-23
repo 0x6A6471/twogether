e4612cd0644360ec26400b38b7ea2ef4e362e671
Jake Quinter <hello@jakequinter.io>
2024-09-23 18:46:01 -0500

    queries

---
+++ client/src/api/guests.ml
@@ -0,0 +1,81 @@
+external api_url : string = "import.meta.env.VITE_API_URL"
+
+type t =
+  { id : string
+  ; user_id : string [@key "userId"]
+  ; first_name : string [@key "firstName"]
+  ; last_name : string [@key "lastName"]
+  ; email : string
+  ; address_line_1 : string [@key "addressLine1"]
+  ; address_line_2 : string option [@key "addressLine2"]
+  ; city : string
+  ; state : string
+  ; zip : string
+  ; country : string
+  ; rsvp_status : string [@key "rsvpStatus"]
+  ; created_at : string [@key "createdAt"]
+  ; updated_at : string [@key "updatedAt"]
+  }
+
+module Decode = struct
+  let guest json =
+    Json.Decode.(
+      json
+      |> fun json ->
+      let id = field "id" string json in
+      let user_id = field "email" string json in
+      let first_name = field "firstName" string json in
+      let last_name = field "lastName" string json in
+      let email = field "email" string json in
+      let address_line_1 = field "addressLine1" string json in
+      let address_line_2 = optional (field "addressLine2" string) json in
+      let city = field "city" string json in
+      let state = field "state" string json in
+      let zip = field "zip" string json in
+      let country = field "country" string json in
+      let rsvp_status = field "rsvpStatus" string json in
+      let created_at = field "createdAt" string json in
+      let updated_at = field "updatedAt" string json in
+      { id
+      ; user_id
+      ; first_name
+      ; last_name
+      ; email
+      ; address_line_1
+      ; address_line_2
+      ; city
+      ; state
+      ; zip
+      ; country
+      ; rsvp_status
+      ; created_at
+      ; updated_at
+      })
+  ;;
+
+  let guests json = Json.Decode.array guest json
+end
+
+let headers =
+  let dict = Js.Dict.empty () in
+  Js.Dict.set dict "Content-Type" "application/json";
+  Fetch.HeadersInit.makeWithDict dict
+;;
+
+let get_user_guests _ =
+  Js.Promise.(
+    Fetch.fetchWithInit
+      (api_url ^ "/api/guests")
+      (Fetch.RequestInit.make () ~credentials:Include ~headers)
+    |> then_ Fetch.Response.json
+    |> then_ (fun json -> Decode.guests json |> resolve))
+;;
+
+let use_guests _ =
+  let open ReactQuery.Query in
+  useQuery
+    (queryOptions
+       ~queryKey:(ReactQuery.Utils.queryKey1 "posts")
+       ~queryFn:get_user_guests
+       ())
+;;
--- client/src/bindings/headless_ui.ml
+++ client/src/bindings/headless_ui.ml
@@ -1,15 +1,18 @@
 module Dialog = struct
-  type role = [ `dialog | `alertdialog ]
+  type role =
+    [ `dialog
+    | `alertdialog
+    ]
 
   external make
-    :  open_:(bool [@mel.as "open"])
+    :  open_:(bool[@mel.as "open"])
     -> onClose:((bool -> bool) -> unit)
-    -> ?as_:(string [@mel.as "as"])
-    -> ?autoFocus: bool
-    -> ?transition: bool
-    -> ?static: bool
-    -> ?unmount: bool
-    -> ?role: role
+    -> ?as_:(string[@mel.as "as"])
+    -> ?autoFocus:bool
+    -> ?transition:bool
+    -> ?static:bool
+    -> ?unmount:bool
+    -> ?role:role
     -> ?children:React.element
     -> ?className:string
     -> React.element
@@ -18,64 +21,63 @@ module Dialog = struct
 end
 
 module DialogBackdrop = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?transition: bool
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?transition:bool
     -> ?className:string
     -> React.element
-   = "DialogBackdrop"
+    = "DialogBackdrop"
   [@@react.component] [@@mel.module "@headlessui/react"]
 end
 
 module DialogPanel = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?transition: bool
-    -> ?className: string
-    -> ?children: React.element
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?transition:bool
+    -> ?className:string
+    -> ?children:React.element
     -> React.element
-  = "DialogPanel"
+    = "DialogPanel"
   [@@react.component] [@@mel.module "@headlessui/react"]
 end
 
 module DialogTitle = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?className: string
-    -> children: React.element
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?className:string
+    -> children:React.element
     -> React.element
     = "DialogTitle"
   [@@react.component] [@@mel.module "@headlessui/react"]
-
-end 
+end
 
 module Description = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?className: string
-    -> children: React.element
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?className:string
+    -> children:React.element
     -> React.element
     = "DialogDescription"
   [@@react.component] [@@mel.module "@headlessui/react"]
 end
 
 module CloseButton = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?className: string
-    -> children: React.element
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?className:string
+    -> children:React.element
     -> React.element
-   = "CloseButton"
+    = "CloseButton"
   [@@react.component] [@@mel.module "@headlessui/react"]
 end
 
 module TransitionChild = struct
-  external make: 
-    ?as_:(string [@mel.as "as"])
-    -> ?appear: bool
-    -> ?unmount: bool
-    -> ?children: React.element
+  external make
+    :  ?as_:(string[@mel.as "as"])
+    -> ?appear:bool
+    -> ?unmount:bool
+    -> ?children:React.element
     -> React.element
-   = "TransitionChild"
+    = "TransitionChild"
   [@@react.component] [@@mel.module "@headlessui/react"]
 end
